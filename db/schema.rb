2c90f0b52fec4dd233469946b34a3cc168489735
Tyler Lu <admin@tyler.lu>
2017-05-19 11:52:02 +0800

    update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -48,8 +48,8 @@ ActiveRecord::Schema.define(version: 20170501145527) do
     t.datetime "created_at",                     null: false
     t.datetime "updated_at",                     null: false
     t.string   "o365_userId"
-    t.string   "refresh_token"
-    t.string   "access_tokens"
+    t.text     "refresh_token"
+    t.text     "access_tokens"
   end
 
   create_table "user_roles", force: :cascade do |t|
