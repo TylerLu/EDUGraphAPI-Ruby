fd5bd9cdfa9008a14517a8df4240f5b191f81851
Tyler Lu <admin@tyler.lu>
2017-05-19 09:34:22 +0800

    update users.js

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -4,126 +4,102 @@
  */
 
 $(document).ready(function() {
-    function i() { $("img[realheader]").each(function(n, t) {
-            var i = $(t);
-            i.attr("src", i.attr("realheader")) }) }
 
-    function t(n, t, i, r, u, f, e) {
-        var o = (t - 1) * 12,
-            s = t * 12,
-            h = e.children();
-        h.slice(o, s).fadeIn("slow", function() {
-            var n = $(this).find("img[realheader]");
-            n.attr("src", n.attr("realheader")) });
-        u.toggleClass("current", !n && s >= h.length && !i);
-        r.toggleClass("current", o === 0);
-        f.val(t) }
+    function loadImages() {
+        $("img[realheader]").each(function (i, e) {
+            var $e = $(e);
+            $e.attr("src", $e.attr("realheader"));
+        });
+    }
+  
+    loadImages();
 
-    function n(n) {
-        var i, r, t;
-        n ? (i = "", $(".txtsearch").val("")) : i = $(".txtsearch").val();
-        r = $(".filterlink-container .selected").text().trim().toLocaleLowerCase();
-        t = "users";
-        switch (r) {
-            case "teachers":
-                t = "teachers";
-                break;
-            case "students":
-                t = "students";
-                break;
-            default:
-                t = "users" }
-        i ? $("#" + t + " .username").each(function() { $(this).text().search(new RegExp(i, "i")) < 0 ? $(this).closest(".element").hide() : $(this).closest(".element").show() }) : $("#" + t + " .element").show() }
-    i();
     $(".teacher-student .filterlink-container .filterlink").click(function() {
-        var t, r, i;
-        n(!0);
-        t = $(this);
-        t.addClass("selected").siblings("a").removeClass("selected");
-        r = t.data("type");
-        i = $(".teacher-student .tiles-root-container");
-        i.removeClass(i.attr("class").replace("tiles-root-container", "")).addClass(r + "-container") });
+        var element = $(this);
+        element.addClass("selected").siblings("a").removeClass("selected");
+        var filterType = element.data("type");
+        var tilesContainer = $(".teacher-student .tiles-root-container");
+        tilesContainer.removeClass(tilesContainer.attr("class").replace("tiles-root-container", "")).addClass(filterType + "-container")         
+    });
+
     $(".teacher-student .tiles-root-container .pagination .prev, .teacher-student .tiles-root-container .pagination .next").on('click', function() {
-        var i, a, r, w, v;
-        if (n(!0), _this = $(this), !_this.hasClass("current") && !_this.hasClass("disabled")) {
-        
-        	var current_page = parseInt(_this.siblings('#curpage').val());
-        	var type = _this.closest(".tiles-secondary-container").attr('id');
-        	if(_this.hasClass('prev')){
-        		
-        		$("#" + type + "_" + current_page).hide();
-        		$("#" + type + "_" + (current_page - 1)).show();
+        var element = $(this);
+        if (element.hasClass("current") || element.hasClass("disabled")) {
+            return;
+        }
 
-        		_this.siblings('#curpage').val(current_page - 1); 
+        var current_page = parseInt(element.siblings('#curpage').val());
+        var type = element.closest(".tiles-secondary-container").attr('id');
+        if(element.hasClass('prev')) {            
+            $("#" + type + "_" + current_page).hide();
+            $("#" + type + "_" + (current_page - 1)).show();
+            element.siblings('#curpage').val(current_page - 1); 
+            if(current_page - 1 <= 1){
+                element.addClass('current');
+            }
+        }
+        else {    
+            var next_page = $('#' + type + '_' + (current_page + 1));
+            var prev_obj = $(".teacher-student .tiles-root-container .pagination .prev");
+            prev_obj.addClass("disabled");
 
-        		if(current_page - 1 <= 1){
-        			_this.addClass('current');
-        		}
-        	}else{
-     
-        		var next_page = $('#' + type + '_' + (current_page + 1));
-                var prev_obj = $(".teacher-student .tiles-root-container .pagination .prev");
-                prev_obj.addClass("disabled");
+            if(next_page.length){
+                $('#' + type + '_' + current_page).hide(); 
+                next_page.show();
 
-        		if(next_page.length){
-					$('#' + type + '_' + current_page).hide(); 
-        			next_page.show();
+                if(current_page + 1 > 1) {
+                    element.siblings('.prev').removeClass('current');
+                }
+                else {
+                    element.siblings('.prev').addClass('current');
+                }
+                element.siblings('#curpage').val(current_page + 1); 
+                prev_obj.removeClass("disabled");
+            }
+            else {
+                var skip_token = element.siblings('#skip_token').val();
+                var school_object_id = $('#school-object-id').val();
+                var school_id = $('#school-id').val();
 
-                    if(current_page + 1 > 1){
-                        _this.siblings('.prev').removeClass('current');
-                    }else{
-                        _this.siblings('.prev').addClass('current');
-                    }
-
-                    _this.siblings('#curpage').val(current_page + 1); 
-                    prev_obj.removeClass("disabled");
-        		}else{
-        			var skip_token = _this.siblings('#skip_token').val();
-	        		var school_object_id = $('#school-object-id').val();
-	        		var school_id = $('#school-id').val();
-
-	        		_this.addClass('disabled');
-	        		$.get({
-	        			url: '/schools/' + school_object_id + '/users_next',
-	        			data: {
-	        				school_id: school_id,
-	        				skip_token: skip_token,
-	        				type: type,
-	        			},
-	        			success: function(res){
-	        				if(res['expired']){
-	        					alert("Your current session has expired. Please click OK to refresh the page.");
-	        					window.location.reload(!1);
-	        					return;
-	        				}
-	        				var html = '<div class="content" id="' +type+ '_'+ (current_page + 1)+'">';
-	        				$.each(res['values'], function(index, user){
-	        					if(user['object_type'] == 'Teacher'){
-	        						html += '<div class="element teacher-bg"><div class="userimg"><img src="/Images/header-default.jpg" realheader="/users/'+ user['object_id'] +'/photo" /></div><div class="username">'+ user['display_name'] +'</div></div>';
-	        					}else{
-	        						html += '<div class="element student-bg"><div class="userimg"><img src="/Images/header-default.jpg" realheader="/users/'+ user['object_id'] +'/photo" /></div><div class="username">'+ user['display_name'] +'</div></div>';
-	        					}
-	        				});
-	        				html += '</div>';
-	        				_this.closest(".tiles-secondary-container").prepend(html);
-	        				_this.siblings('#skip_token').val(res['skip_token']);
-	        				_this.removeClass('disabled');
-                            $('#'+ type +'_' + current_page).hide();
-
-                            if(current_page + 1 > 1){
-                                _this.siblings('.prev').removeClass('current');
+                element.addClass('disabled');
+                $.get({
+                    url: '/schools/' + school_object_id + '/users_next',
+                    data: {
+                        school_id: school_id,
+                        skip_token: skip_token,
+                        type: type,
+                    },
+                    success: function(res){
+                        if(res['expired']){
+                            alert("Your current session has expired. Please click OK to refresh the page.");
+                            window.location.reload(!1);
+                            return;
+                        }
+                        var html = '<div class="content" id="' +type+ '_'+ (current_page + 1)+'">';
+                        $.each(res['values'], function(index, user){
+                            if(user['object_type'] == 'Teacher'){
+                                html += '<div class="element teacher-bg"><div class="userimg"><img src="/Images/header-default.jpg" realheader="/users/'+ user['object_id'] +'/photo" /></div><div class="username">'+ user['display_name'] +'</div></div>';
                             }else{
-                                _this.siblings('.prev').addClass('current');
+                                html += '<div class="element student-bg"><div class="userimg"><img src="/Images/header-default.jpg" realheader="/users/'+ user['object_id'] +'/photo" /></div><div class="username">'+ user['display_name'] +'</div></div>';
                             }
+                        });
+                        html += '</div>';
+                        element.closest(".tiles-secondary-container").prepend(html);
+                        element.siblings('#skip_token').val(res['skip_token']);
+                        element.removeClass('disabled');
+                        $('#'+ type +'_' + current_page).hide();
 
-                            _this.siblings('#curpage').val(current_page + 1); 
-                            prev_obj.removeClass("disabled");
-	        			}
-	        		})
-        		}
-        	}
-          } 
-      });
-    $("#btnsearch").click(function() { n() });
-    $(".txtsearch").on("keypress", function(t) { t.which === 13 && n() })
+                        if(current_page + 1 > 1){
+                            element.siblings('.prev').removeClass('current');
+                        }else{
+                            element.siblings('.prev').addClass('current');
+                        }
+
+                        element.siblings('#curpage').val(current_page + 1); 
+                        prev_obj.removeClass("disabled");
+                    }
+                })
+            }
+        }          
+    })
 })
