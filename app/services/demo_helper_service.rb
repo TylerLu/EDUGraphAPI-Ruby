f454b223254ee7aa3cb820a152494d61f764dd3b
Tyler Lu <admin@tyler.lu>
2017-05-19 08:57:03 +0800

    demo helper

--- app/services/demo_helper_service.rb
+++ app/services/demo_helper_service.rb
@@ -5,17 +5,162 @@ class DemoHelperService
 
   @@data = [
     {
+      'controller': 'account',
+      'action': 'login',
+      'links': {
+        'AccountController': '/app/controllers/account_controller.rb',
+        'TokenService': '/app/services/token_service.rb',
+        'MSGraphService': '/app/services/ms_graph_service.rb',
+        'UserService': '/app/services/user_service.rb',
+        'OrganizationService': '/app/services/organization_service.rb',
+        'LoginView': '/app/views/account/login.html.erb',
+        'O365LoginView': '/app/views/account/o365login.html.erb',
+      }
+    },
+    {
+      'controller': 'account',
+      'action': 'register',
+      'links': {
+        'AccountController': '/app/controllers/account_controller.rb',
+        'UserService': '/app/services/user_service.rb',
+        'View': '/app/views/account/register.html.erb',
+      }
+    },
+    {
+      'controller': 'link',
+      'action': 'index',
+      'links': {
+        'LinkController': '/app/controllers/link_controller.rb',
+        'UserService': '/app/services/user_service.rb',
+        'LinkService': '/app/services/link_service.rb',
+        'TokenService': '/app/services/token_service.rb',
+        'MSGraphService': '/app/services/ms_graph_service.rb',
+        'View': '/app/views/link/index.html.erb',
+      }
+    },
+    {
+      'controller': 'link',
+      'action': 'create_local',
+      'links': {
+        'LinkController': '/app/controllers/link_controller.rb',
+        'UserService': '/app/services/user_service.rb',
+        'LinkService': '/app/services/link_service.rb',
+        'View': '/app/views/link/create_local.html.erb',
+      }
+    },
+    {
+      'controller': 'link',
+      'action': 'login_local',
+      'links': {
+        'LinkController': '/app/controllers/link_controller.rb',
+        'UserService': '/app/services/user_service.rb',
+        'LinkService': '/app/services/link_service.rb',
+        'View': '/app/views/link/login_local.html.erb',
+      }
+    },
+    {
+      'controller': 'link',
+      'action': 'login_o365_required',
+      'links': {
+        'LinkController': '/app/controllers/link_controller.rb',
+        'View': '/app/views/link/login_o365_required.html.erb',
+      }
+    },
+    {
+      'controller': 'admin',
+      'action': 'index',
+      'links': {
+        'AdminController': '/app/controllers/admin_controller.rb',
+        'OrganizationService': '/app/services/organization_service.rb',
+        'TokenService': '/app/services/token_service.rb',
+        'AADGraphService': '/app/services/aad_graph_service.rb',
+        'View': '/app/views/admin/index.html.erb',
+      }
+    },
+    {
+      'controller': 'admin',
+      'action': 'consent',
+      'links': {
+        'AdminController': '/app/controllers/admin_controller.rb',
+        'OrganizationService': '/app/services/organization_service.rb',
+        'View': '/app/views/admin/consent.html.erb',
+      }
+    },
+    {
+      'controller': 'admin',
+      'action': 'linked_accounts',
+      'links': {
+        'AdminController': '/app/controllers/admin_controller.rb',
+        'LinkService': '/app/services/link_service.rb',
+        'View': '/app/views/admin/linked_accounts.html.erb',
+      }
+    },
+    {
+      'controller': 'admin',
+      'action': 'unlink_account',
+      'links': {
+        'AdminController': '/app/controllers/admin_controller.rb',
+        'LinkService': '/app/services/link_service.rb',
+        'View': '/app/views/admin/unlink_account.html.erb',
+      }
+    },
+    {
+      'controller': 'manage',
+      'action': 'aboutme',
+      'links': {
+        'ManageController': '/app/controllers/manage_controller.rb',
+        'UserService': '/app/services/user_service.rb',
+        'EducationService': '/lib/education/education_service.rb',
+        'View': '/app/views/manage/aboutme.html.erb',
+      }
+    },
+    {
       'controller': 'schools',
       'action': 'index',
       'links': {
-        'AccountController': '/app/controllers/schools_controller.rb'
+        'SchoolsController': '/app/controllers/schools_controller.rb',
+        'TokenService': '/app/services/token_service.rb',
+        'EducationService': '/lib/education/education_service.rb',
+        'BingMapService': '/app/services/bing_map_service.rb',        
+        'View': '/app/views/schools/index.html.erb',
+      }
+    },
+    {
+      'controller': 'schools',
+      'action': 'users',
+      'links': {
+        'SchoolsController': '/app/controllers/schools_controller.rb',
+        'TokenService': '/app/services/token_service.rb',
+        'EducationService': '/lib/education/education_service.rb',    
+        'View': '/app/views/schools/users.html.erb',
+      }
+    },
+    {
+      'controller': 'classes',
+      'action': 'index',
+      'links': {
+        'ClassesController': '/app/controllers/classes_controller.rb',
+        'TokenService': '/app/services/token_service.rb',
+        'EducationService': '/lib/education/education_service.rb',    
+        'View': '/app/views/classes/index.html.erb',
+      }
+    },
+    {
+      'controller': 'classes',
+      'action': 'show',
+      'links': {
+        'ClassesController': '/app/controllers/classes_controller.rb',
+        'TokenService': '/app/services/token_service.rb',
+        'EducationService': '/lib/education/education_service.rb',
+        'MSGraphService': '/app/services/ms_graph_service.rb',
+        'UserService': '/app/services/user_service.rb',
+        'View': '/app/views/classes/show.html.erb',
       }
     }
   ]
-  # TODO fill data helper data
 
   def self.get_links(controller, action)        
-    item = @@data.first{ |item| item[:controller] == controller && item[:action] == action }
+    item = @@data.detect{ |item| item[:controller] == controller && item[:action] == action }
     item ? item[:links] : {}
   end
 
