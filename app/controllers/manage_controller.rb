12b7fa7ce15542ff5d1a4f80cebdecd8d77d023c
TylerLu <admin@tyler.lu>
2017-12-19 16:59:32 +0800

    Format ManageController

--- app/controllers/manage_controller.rb
+++ app/controllers/manage_controller.rb
@@ -6,20 +6,20 @@ class ManageController < ApplicationController
   before_action :require_login
 
   def aboutme
-		if current_user.are_linked? or current_user.local_user
-			user_service = UserService.new
-			@favorite_color = user_service.get_favorite_color(current_user.user_id)
-		end
-		if current_user.o365_user
-			token = token_service.get_access_token(current_user.o365_user_id, Constants::Resources::MSGraph)
-			education_service = Education::EducationService.new(current_user.tenant_id, token)
-			@classes = education_service.get_my_classes()
-		end
+    if current_user.are_linked? or current_user.local_user
+      user_service = UserService.new
+      @favorite_color = user_service.get_favorite_color(current_user.user_id)
+    end
+    if current_user.o365_user
+      token = token_service.get_access_token(current_user.o365_user_id, Constants::Resources::MSGraph)
+      education_service = Education::EducationService.new(current_user.tenant_id, token)
+    @classes = education_service.get_my_classes()
+    end
   end
 
   def update_favorite_color
-		user_service = UserService.new
-		user_service.update_favorite_color(params["user_id"], params["favoritecolor"])
+    user_service = UserService.new
+    user_service.update_favorite_color(params["user_id"], params["favoritecolor"])
   	redirect_to manage_aboutme_path, notice: 'Favorite color has been updated!'
   end
 
